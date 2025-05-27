732f212edb5b687ba95d4bd26e9557f1c6108dd1
ano <ano_y@tokium.jp>
2025-05-27 16:18:19 +0900

    topページのヘッダーを消しました

--- frontend/src/app/top/page.tsx
+++ frontend/src/app/top/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
 
 export default async function TopPage() {
   const session = await getServerSession(authOptions);
@@ -10,7 +10,7 @@ export default async function TopPage() {
   }
   return (
     <>
-      <Navbar />
+      {/*<Navbar /> */}
       <div className="container py-5">
         <h1 className="display-4 mb-4">社内限定TOPページ</h1>
         <p>ようこそ、{session.user?.email} さん！</p>
