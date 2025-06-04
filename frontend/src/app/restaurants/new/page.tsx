069effbed830a9c160005f370035a6e4c2bed8db
阿野庸太郎 <ano_y@example.com>
2025-06-04 10:03:13 +0900

    タスク 4.2: 【FE】店舗登録ページ作成、ルーティング設定

---
+++ frontend/src/app/restaurants/new/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RestaurantNewPage = () => {
+  return (
+    <div>
+      <h1>店舗登録フォーム</h1>
+      {/* ここにフォームUIを追加していきます */}
+    </div>
+  );
+};
+
+export default RestaurantNewPage;
